49471a77f81eba8997c411af2c7f9e31fb2e371a
vishwas-trivedi <vishwas.trivedi2012@gmail.com>
2019-11-28 14:56:33 +0530

    added sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,20 +8,37 @@ class SessionsController < ApplicationController
   # Used to verify user login details
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      flash[:success] = 'You have successfully logged in.'
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    message = 'You have successfully logged in.'
+    if user
+      if user.authenticate(params[:session][:password])
+        flash[:success] = message
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        message = 'Invalid log in credentials'
+        flash.now[:danger] = message
+        render 'new'
+      end
     else
-      flash.now[:danger] = 'Something is wrong with you login information'
+      message = 'User with given email address does not exist.'
+      flash.now[:danger] = message
       render 'new'
     end
+
+  #   if user && user.authenticate(params[:session][:password])
+  #     flash[:success] = 'You have successfully logged in.'
+  #     session[:user_id] = user.id
+  #     redirect_to user_path(user)
+  #   else
+  #     flash.now[:danger] = 'Something is wrong with you login information'
+  #     render 'new'
+  #   end
   end
 
   # Used to logout user
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have logged out."
+    flash[:success] = 'You have logged out.'
     redirect_to root_path
   end
 end
