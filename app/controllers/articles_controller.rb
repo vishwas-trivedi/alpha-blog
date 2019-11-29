dde49345f193df90706da7d7f70d7d650a9ac803
vishwas-trivedi <vishwas.trivedi2012@gmail.com>
2019-11-29 17:05:32 +0530

    Added user operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # Listing with index action
   def index
@@ -15,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
@@ -56,5 +58,12 @@ class ArticlesController < ApplicationController
     # Whitelisting or strong parameters
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = 'You can only edit or delete your own article.'
+        redirect_to root_path
+      end
     end
 end
